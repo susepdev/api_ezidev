4401906575ef14adef59b354dafb2d44e4b0e3a4
muzibmuzakar <muzibmuzakar@gmail.com>
2023-02-25 12:52:30 +0700

    add crud sla_resolution

---
+++ app/Http/Controllers/SlaResolutionController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SlaResolutionResource;
+use App\Models\SlaResolution;
+use Illuminate\Http\Request;
+
+class SlaResolutionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Data SLA Resolution',
+            'date' => SlaResolutionResource::collection(SlaResolution::all())
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'desc' => 'required',
+            'is_active' => 'required',
+            'updated_by' => 'required',
+        ]);
+
+        $input = SlaResolution::create($data);
+
+        return response()->json([
+            'message' => 'Create SLA Resolution Success',
+            'data' => $input
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return new SlaResolutionResource(SlaResolution::FindOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'desc' => 'required',
+            'is_active' => 'required',
+            'updated_by' => 'required',
+        ]);
+
+        $input = SlaResolution::where('id', $id)->update($data);
+
+        return response()->json([
+            'message' => 'Update SLA Resolution Success'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        SlaResolution::where('id', $id)->delete();
+
+        return response()->json([
+            'message' => 'Delete SLA Resolution Success'
+        ]);
+    }
+}
