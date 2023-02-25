38bc6c5529e6515690a56fe330a66b68848546bf
muzibmuzakar <muzibmuzakar@gmail.com>
2023-02-25 20:24:16 +0700

    add crud pm_code

---
+++ app/Http/Controllers/PmCodeController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PmCodeResource;
+use App\Models\PmCode;
+use Illuminate\Http\Request;
+
+class PmCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Data PM Code',
+            'date' => PmCodeResource::collection(PmCode::all())
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'desc' => 'required',
+            'is_active' => 'required',
+            'updated_by' => 'required',
+        ]);
+
+        $input = PmCode::create($data);
+
+        return response()->json([
+            'message' => 'Create PM Code Success',
+            'data' => $input
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return new PmCodeResource(PmCode::FindOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'desc' => 'required',
+            'is_active' => 'required',
+            'updated_by' => 'required',
+        ]);
+
+        $input = PmCode::where('id', $id)->update($data);
+
+        return response()->json([
+            'message' => 'Update PM Code Success'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        PmCode::where('id', $id)->delete();
+
+        return response()->json([
+            'message' => 'Delete PM Code Success'
+        ]);
+    }
+}
